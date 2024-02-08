90e9c0342c966b14bb956926cdca21194e8e57cb
paolo flore <paoloflore937@gmail.com>
2024-02-08 18:47:20 +0100

    variabbili

---
+++ variabbili.php
@@ -0,0 +1,24 @@
+<?php
+$variabbile = 30 ;
+$variabbileStringa = "ciao";
+$variabbile0 = false;
+$variabbile1 = true;
+$prova = ["ciao" , "mi" , "chiamo" , "paolo" ,"e sono nato il" , 1999 , 10.07 , true ];
+
+echo $variabbile;
+echo $variabbileStringa;
+var_dump($prova)
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ciao mondo</h1>
+</body>
+</html>
