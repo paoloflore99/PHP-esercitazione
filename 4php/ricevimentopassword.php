b50c3087492875b5c1f258695c963c9f5b8046ca
paolo flore <paoloflore937@gmail.com>
2024-03-06 00:06:58 +0100

    contio domani

--- 4php/ricevimentopassword.php
+++ 4php/ricevimentopassword.php
@@ -52,43 +52,39 @@ function PasswordGenerara($lunghezza) {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="icon" type="image/x-icon" href="https://boolean.careers/favicon/favicon.ico">
-        <!-- <link rel="stylesheet" href="css/style.css">css -->
+        <link rel="stylesheet" href="../4php/CSS/style.css"><!--css-->
         <title>php</title>
 </head>
 <body>
-<nav class="container text-center p-4">
+<nav class=" text-center p-4">
     <h1>invio dati form per la pssword</h1>
 </nav>
-<div class="container">
+<div class="">
     <p>ciao <?php echo $nome ?> <?php echo $cognome ?></p>
     <p>la tua password e <strong><?php echo $password ?></strong></p>
+    <a href="eserciziopassword.php">Indietro</a>
 </div>
 
 
-<div class="container p-3">
+<div class=" p-3">
     <h3>come ho fatto a creara un generatore di password</h3>
 </div>
 
-
-
-</body>
-</html>
-    <div class="container">
+<div class="">
         <H2>creazione password automatica</H2>
     </div>
 
 
-    <div class="container">
+    <div class="">
         <ul>
 
             <li>
                 <strong>Dichiaro la funzione:</strong>
                 <p>Allinterno della funzione richiamo le variabbili </p>
                 <pre>
-                    <code>
-                        function PasswordGenerara($lunghezza) {
-                        }
-                    </code>
+                    
+<code>function PasswordGenerara($lunghezza) { ...}</code>
+                    
                 </pre>
             </li> 
 
@@ -97,23 +93,31 @@ function PasswordGenerara($lunghezza) {
                 <strong>variabbili:</strong>
                 <p>Creo tre variabbili allintero della funzione : lettere minuscole maiuscole e numeri.</p>
                 <pre>
-                    <code>   
-                        $simboli = ["!", "@", "#", "$", "%", "^", "&", "(", ")", "_", "-", "=", "+", "?" ];
-                        $lettereMinuscole = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "l", "m", "n"];
-                        $lettereMaiuscole = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "Q", "R"];
-                        $random = [1, 1 , 2, 3 ,4, 5 ,6 ,7 ,8 ,9 ];</code>
+<code>
+$simboli = ["!", "@", "#", "$", "%", "^", "&", "(", ")", "_", "-", "=", "+", "?" ];
+$lettereMinuscole = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "l", "m", "n"];
+$lettereMaiuscole = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "L", "M", "N", "O", "P", "Q", "R"];
+$random = [1, 1 , 2, 3 ,4, 5 ,6 ,7 ,8 ,9 ];</code>
                 </pre>
             </li>
 
             
             
             <li>
+                <strong>Ciclo for:</strong>
+                <p></p>
+                <pre>
+<code>for ($i = 0; $i < $lunghezza; $i++) {}</code>
+                </pre>
 
-            
             </li>
         </ul>
     </div>
 
+
+</body>
+</html>
+   
 
 
 <!-- for ($i = 0; $i < $lunghezza; $i++): Questo è un ciclo for che si ripeterà per il numero di volte specificato dalla variabile $lunghezza. In ogni iterazione, il blocco di codice all'interno del ciclo verrà eseguito.
