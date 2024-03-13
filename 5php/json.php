d993e996c68aadd5882ce592396d8adb491d54db
paolo flore <paoloflore937@gmail.com>
2024-03-13 19:44:14 +0100

    decode encore

--- 5php/json.php
+++ 5php/json.php
@@ -18,4 +18,18 @@ echo json_encode($esmpio);
 
 // per dare invece un arrey si usa
 //se voglioo convvertire un json in arrey uso (   json_decode($json , true) )
+
+
+//leggo il contenuto del fil in tipologia stringa 
+$esempio = file_get_contents("../5php/db/esmpio.json");
+var_dump($esempio);
+
+
+
+//conveto la stringa(prima covertita da arrey a json ) in arrey , grazie al (   json_decode($json , true) )
+$dajsonaarrey = json_decode($esempio , true);
+var_dump($dajsonaarrey);
+
+
 ?>
+
