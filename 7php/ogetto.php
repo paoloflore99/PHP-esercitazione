e8b3b214b0c48f2014ab8524ea2693827a9d6704
paolo flore <paoloflore937@gmail.com>
2024-04-09 17:03:26 +0200

    piccolo esempio di ogetti e il funzionamento di funzioni allinterno

--- 7php/ogetto.php
+++ 7php/ogetto.php
@@ -1,19 +1,48 @@
 <?php
 
-class user {
 
+class Macchina
+{
+        public $colore;
+        public $modello;
+        public $costo;
+
+        public function differenza($auto1, $auto2)
+        {
+                if ($auto1->costo < $auto2->costo) {
+                        var_dump($auto1);
+                } else {
+                        var_dump($auto2);
+                }
+        }
 }
 
 
-$mario = new user();
+
+$audi = new Macchina();
+$audi->modello = "serie 7";
+$audi->colore = "verde";
+$audi->costo = 20000;
+
+$mercedes = new Macchina();
+$mercedes->modello = "classe a";
+$mercedes->colore = "nera";
+$mercedes->costo = 24800;
+
+$macchina = new Macchina();
+$macchina->differenza($audi, $mercedes);
+//la base rimane allinterno della classe macchina
+//lasegnazione di oggni valore va fatto fuiri la clsse graziue al ->
+//mel casop io nom asegno un valore dichiato allinterno della clsse , il valore sara null
 ?>
 
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <meta charset="UTF-8">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -21,7 +50,9 @@ $mario = new user();
         <link rel="stylesheet" href="../4php/CSS/style.css"><!--css-->
         <title>php</title>
 </head>
+
 <body>
 
 </body>
+
 </html>
